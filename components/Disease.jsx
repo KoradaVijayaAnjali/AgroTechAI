cb5371ee9ae64f415e43a87e3c4f5dcc7625366f
Korada Vijaya Anjali <97424576+KoradaVijayaAnjali@users.noreply.github.com>
2024-08-11 02:09:52 +0530

    Add files via upload

---
+++ components/Disease.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import Spinner from './Spinner';
+import { Link } from 'react-router-dom';
+
+const Disease = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  const fruits = [
+    { name: 'Apple', image: 'https://post.healthline.com/wp-content/uploads/2020/09/Do_Apples_Affect_Diabetes_and_Blood_Sugar_Levels-732x549-thumbnail-1-732x549.jpg' },
+    { name: 'Blueberry', image: 'https://www.supermarketperimeter.com/ext/resources/0430-blueberries.png?t=1588260305&width=1080' },
+    { name: 'Cherry', image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/slideshows/health_benefits_of_cherries_slideshow/1800x1200_health_benefits_of_cherries_slideshow.jpg' },
+    { name: 'Corn', image: 'https://m.media-amazon.com/images/I/41F62-VbHSL._AC_UF1000,1000_QL80_.jpg' },
+    { name: 'Grape', image: 'https://i.ndtvimg.com/i/2015-09/grapes_625x350_61443376353.jpg' },
+    { name: 'Orange', image: 'https://www.irishtimes.com/polopoly_fs/1.3923226.1560339148!/image/image.jpg_gen/derivatives/ratio_1x1_w1200/image.jpg' },
+    { name: 'Peach', image: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/the_health_benefits_of_peaches_slideshow/thinkstock_rf_peaches.jpg?resize=650px:*' },
+    { name: 'Pepper Bell', image: 'https://upload.wikimedia.org/wikipedia/commons/8/85/Green-Yellow-Red-Pepper-2009.jpg' },
+    { name: 'Potato', image: 'https://m.economictimes.com/thumb/height-450,width-600,imgsize-111140,msid-72862126/potato-getty.jpg' },
+    { name: 'Raspberry', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/326/326272/raspberries-with-ketones-in-a-bowl.jpg?w=1155&h=1541' },
+    { name: 'Soybean', image: 'https://m.economictimes.com/thumb/msid-66988154,width-1200,height-900,resizemode-4,imgsize-211276/soyabean-agencies.jpg' },
+    { name: 'Squash', image: 'https://post.healthline.com/wp-content/uploads/2020/08/squash-fruit-or-vegetable-732x549-thumbnail-732x549.jpg' },
+    { name: 'Strawberry', image: 'https://images.indianexpress.com/2020/02/strawberry-1200.jpg' },
+    { name: 'Tomato', image: 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/tomatoes-1296x728-feature.jpg' },
+  ];
+
+  return (
+    <>
+      {loading ? <Spinner /> : <div className="max-w-full mt-16 mx-auto px-4 pb-10 pt-5 sm:px-6 lg:px-8">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-green-500">🍁Plant Disease Detection🍁</h1>
+          <p className="text-xl text-gray-700 mt-2">This AI Engine Will Help To Detect Disease From Following Fruits And Veggies</p>
+        </div>
+        <div className="text-center mb-8">
+          <Link to="/engine" className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
+            AI Engine
+          </Link>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {fruits.map((fruit, index) => (
+            <div key={index} className="bg-white shadow rounded-lg p-4">
+              <img src={fruit.image} alt={fruit.name} className="w-full h-48 object-cover rounded-lg mb-4" />
+              <p className="text-center font-medium text-gray-800">{fruit.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>}
+    </>
+
+
+  );
+};
+
+export default Disease;
---
+++ components/Footer.jsx
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+import { FaHome, FaGithub, FaRegCopyright } from 'react-icons/fa';
+import playstore from "../assets/favicon2.png";
+// import bgHero from "../assets/bgHero.png";
+
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
+    return (
+        <div className='bg-gradient-to-r from-[#11cb46] via-[#3ff132] to-[#04ba10]  p-6'>
+            <div className='md:flex'>
+                <div className="md:w-1/2 md:-mt-10 sm:mt-1 sm:w-full content-center">
+                    <div className="mt-1 sm:mt-2 md:mt-10 flex flex-col md:items-start">
+                        <div className="flex items-center sm:w-full">
+                            <Link to="/" className="flex items-center gap-1">
+                                <img src={playstore} className="h-8" alt="AgroTech AI Logo" />
+                                <span className="ml-4 text-2xl font-semibold whitespace-nowrap text-white">
+                                    AgroTech AI
+                                </span>
+                            </Link>
+                        </div>
+                        <p className="mt-2 text-white text-justify text-sm md:text-base md:text-left  lg:w-1/2 sm:w-full pr-2">
+                            AgroTech AI platform provides various Machine Learning models for predictions.
+                        </p>
+                    </div>
+                </div>
+                <div className='md:w-3/4 md:flex'>
+                    <div className='md:w-1/3'>
+                        <p className='text-white font-bold mt-8 md:mt-0'>Company</p>
+                        <ul className='text-white py-1'>
+                            <li className='py-0.5 cursor-pointer hover:text-green-600'>
+                                <Link to='/'>About Us</Link>
+                            </li>
+                            <li className='py-0.5 cursor-pointer hover:text-green-600'>
+                                <Link to='/'>Contact Us</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='md:w-1/3'>
+                        <p className='text-white font-bold text-md'>Quick Links</p>
+                        <ul className='text-white py-1 cursor-pointer'>
+                            {/* <li className='py-1 cursor-pointer hover:text-green-800'>
+                                <Link className='flex items-center gap-1' to='/crop'>
+                                    Crop Recommendation 
+                                </Link>
+                            </li> */}
+                            {/* <li className='py-1 cursor-pointer hover:text-green-800'>
+                                <Link className='flex items-center gap-1' to='/fertilizer'>
+                                    Fertilizer Recommendation
+                                </Link>
+                            </li> */}
+                            {/* <li className='py-0.5 cursor-pointer hover:text-green-600'>
+                                <Link className='flex items-center gap-1' to='/soil'>
+                                    Soil Quality 
+                                </Link>
+                            </li> */}
+                            <li className='py-0.5 cursor-pointer hover:text-green-600'>
+                                <Link className='flex items-center gap-1' to='/prices'>
+                                    Price Prediction
+                                </Link>
+                            </li>
+                            <li className='py-0.5 cursor-pointer hover:text-green-600'>
+                                <Link className='flex items-center gap-1' to="/forecast">
+                                   Forecast
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* <div className='md:w-1/3'>
+                        <p className='text-white font-bold'>Legal Pages</p>
+                        <ul className='text-white py-4 cursor-pointer'>
+                            <li className='py-1 cursor-pointer hover:text-green-800'>
+                                <Link to='/'>Privacy Policy</Link>
+                            </li>
+                            <li className='py-1 cursor-pointer hover:text-green-800'>
+                                <Link to='/'>Terms and Conditions</Link>
+                            </li>
+                            <li className='py-1 cursor-pointer hover:text-green-800'>
+                                <Link to='/'>Cookie Policy</Link>
+                            </li>
+                        </ul>
+                    </div> */}
+                </div>
+            </div>
+            {/* <div className='block items-center m-auto mt-2'>
+                <div className='flex gap-4 justify-center '>
+                    <a href="https://github.com/manikumarreddyu/AGRO-AI" className="hover:scale-110 hover:shadow-[0_0_10px_2px_green] transition-transform duration-300 ease-in-out flex items-center justify-center p-2 rounded-full">
+                        <FaGithub size={35} className="text-white" />
+                    </a>
+                    <Link to='/' className="hover:scale-110 hover:shadow-[0_0_10px_2px_green] transition-transform duration-300 ease-in-out flex items-center justify-center p-2 text-white rounded-full focus:outline-none">
+                        <FaHome size={35} />
+                    </Link>
+                </div>
+            </div> */}
+            <div className='mt-4 text-white flex justify-center space-x-2 text-md lg:text-md xl:text-md'>
+                Copyright <FaRegCopyright className="mx-0.5 my-1.5" /> {currentYear} <span className="font-bold">AgroTech AI</span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
+
