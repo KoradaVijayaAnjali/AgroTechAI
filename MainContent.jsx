253336ad8d50f333fb83daf2e95bbfb7f54d0e8d
Korada Vijaya Anjali <97424576+KoradaVijayaAnjali@users.noreply.github.com>
2024-08-11 02:10:59 +0530

    Add files via upload

---
+++ MainContent.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Crop from './components/CropRecommendation';
+import Disease from './components/Disease';
+import AIEngine from './components/AIEngine';
+import Submit from './components/Submit';
+import Home from './pages/Home';
+import Fertilizer from './components/Fertilizer';
+import SoilQuality from './components/SoilQuality';
+import Footer from './components/Footer';
+import GoTop from './components/GoTop';
+import Forecast from './pages/Forecast';
+import Prices from './components/Prices';
+import Reports from './components/Reports';
+import UseScrollToTop from './components/UseScrollToTop';
+
+const MainContent = () => {
+  UseScrollToTop();
+
+  return (
+    <div>
+      <GoTop />
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/crop" element={<Crop />} />
+          <Route path="/disease" element={<Disease />} />
+          <Route path="/engine" element={<AIEngine/>} />
+          <Route path="/submit" element={<Submit/>} />
+          <Route path="/fertilizer" element={<Fertilizer />} />
+          <Route path="/soil" element={<SoilQuality />} />
+          <Route path="/forecast" element={<Forecast />} />
+          <Route path="/prices" element={<Prices />} />
+          <Route path="/reports" element={<Reports />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
